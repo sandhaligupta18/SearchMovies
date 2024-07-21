cb6114248646a8488676ecf4b1722081dea14c0e
sandhaligupta18 <117626843+sandhaligupta18@users.noreply.github.com>
2024-07-21 10:01:09 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // import { useCallback, useState } from 'react'
-import { FaSearch } from "react-icons/fa";
+
 // import './App.css'
 import { useEffect, useState } from 'react';
 import MovieCard from "./MovieCard";
